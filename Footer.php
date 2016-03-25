a3aa7ebfbd8341e93166fb1acb6d37a4b7fefbbe
cladlink <michael.boutboul87@gmail.com>
2016-03-25 08:40:31 +0100

    Requetes ok table ok foundation css + index  + header + footer

---
+++ Footer.php
@@ -0,0 +1,37 @@
+<a href="#" class="scroll"><img src="images/Scroll-to-top-button.png" alt="Fleche vers le haut" /></a>
+<footer>
+	<div>
+		<div>Site Web créé par : Michael BOUTBOUL	-   Marie-Lucile CANIARD<br/><br/></div>
+	</div>
+</footer>
+	<script src="js/vendor/smoothscroll.js"></script>
+	<script src="js/vendor/jquery.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('body').append('<a href="#top" class="top_link" title="Revenir en haut de page">Haut</a>');
+		});
+	</script>
+	<script>
+		$(document).ready(function(){
+			
+			//Check to see if the window is top if not then display button
+			$(window).scroll(function(){
+				if ($(this).scrollTop() > 100) {
+					$('.scroll').fadeIn();
+				} else {
+					$('.scroll').fadeOut();
+				}
+			});
+			
+			//Click event to scroll to top
+			$('.scroll').click(function(){
+				$('html, body').animate({scrollTop : 0},800);
+				return false;
+			});
+			
+		});
+	</script>
+    <script src="js/foundation.min.js"></script>
+    <script> $(document).foundation();</script>
+	</body>
+</html>
---
+++ Header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fr">
+	<head>
+		<title>Projet tuteuré</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta charset="utf-8">
+		<link rel="icon" href="images/favicon.ico" />
+		<link rel="stylesheet" href="css/foundation.css" />
+		<link rel="stylesheet" href="css/foundation.min.css" />
+		<link rel="stylesheet" type="text/css" href="Styles.css">
+
+		<script src="js/vendor/modernizr.js"></script>
+	</head>
+	<body>
+		<header>
+			<div class="fixed" >
+				<nav class="top-bar menu " data-topbar data-options="sticky_on: medium">
+				  <ul class="title-area">
+				    <li class="name">
+				      <h1><a href="index.php">Azerti</a></h1>
+				    </li>
+				    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+				  </ul>
+				  <div class="top-bar-section">
+				    <ul class="center">
+			            <li class="active"><a href="PresentationAzerti.php">Emprunt</a></li>
+						<li class="active"><a href="Eco.php">Oeuvre</a></li>
+			            <li class="active"><a href="Interview.php">Interview</a></li>
+						<li class="active"><a href="Echecs.php">Echecs</a></li>
+						<li class="active"><a href="Contact.php">Contact</a></li>
+						<li class="active"><a href="Remerciement.php">Remerciements</a></li>
+			        </ul>
+				  </div>
+				</nav>
+			</div>
+		</header>
