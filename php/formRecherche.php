a8ba7bb0a76ae8b02cf96a63fa4a85de9c49d1b4
cladlink <michael.boutboul87@gmail.com>
2016-03-29 10:44:44 +0200

    Requetes ok table ok foundation css + index  + header + footer

--- php/formRecherche.php
+++ php/formRecherche.php
@@ -1,8 +1,33 @@
 <?php
-    if(isset($_POST) && isset($_POST['nomAuteur']) && isset($_POST['titreOeuvre']) && isset($_POST['dateParution']))
-    {
-        if(!empty($_POST['nomAuteur']) && !empty($_POST['titreOeuvre']) && !empty($_POST['dateParution']))
-        {
+/**
+ * Created by PhpStorm.
+ * User: cladlink
+ * Date: 29/03/16
+ * Time: 09:44
+ */
+                define("username","cladlink");
+                define("password","Tr1f0rc31987..");
+                $dsn = 'mysql:dbname=tpsql;host=193.253.204.231:12269;charset=utf8';
 
-        }
-    }
+                try
+                {
+                    $ma_connexion_mysql = new PDO($dsn, username, password);
+                    // pour afficher les erreurs
+                    $ma_connexion_mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    // pour récupérer le résultat des requêtes SELECT sous forme de tableaux associatifs
+                    $ma_connexion_mysql->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+                }
+                catch (PDOException $e)
+                {
+                    echo 'Connexion échouée : ' . $e->getMessage();
+                }
+
+            $ma_commande_SQL = "SELECT * FROM AUTEUR;";
+            $reponse = $ma_connexion_mysql->query($ma_commande_SQL);
+            $donnees = $reponse->fetchAll();
+            echo "<table><thead><td>nom étudiant</td><td>sexe</td><td>Adresse</td></thead>";
+            foreach ($donnees as $row)
+            {
+                echo "<tr><td>" . $row['idAuteur'] . "</td><td>" . $row['nomAuteur'] . "</td><td>" . $row['prenomAuteur'] . "</tr></td>";
+            }
+            echo "</table>";
---
+++ php/secu.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cladlink
+ * Date: 29/03/16
+ * Time: 09:44
+ */
+define("hostname","193.253.204.231");
+define("database","TPSQL");
+define("username","cladlink");
+define("password","Tr1f0rc31987..");
+$dsn = 'mysql:dbname='.database.';host='.hostname.';charset=utf8';
