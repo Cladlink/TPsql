6a69c4de212ec0127ea53fd38cab8fa548835874
cladlink <michael.boutboul87@gmail.com>
2016-04-01 08:05:51 +0200

    Requetes ok table ok foundation css + index  + header + footer formRecherche V1.0

--- php/secu.php
+++ php/secu.php
@@ -5,8 +5,5 @@
  * Date: 29/03/16
  * Time: 09:44
  */
-define("hostname","193.253.204.231");
-define("database","TPSQL");
 define("username","cladlink");
 define("password","Tr1f0rc31987..");
-$dsn = 'mysql:dbname='.database.';host='.hostname.';charset=utf8';
