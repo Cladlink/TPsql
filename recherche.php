5306fa6fa92c94a6abd05367d9a33036b23126ff
cladlink <michael.boutboul87@gmail.com>
2016-04-01 08:05:25 +0200

    Requetes ok table ok foundation css + index  + header + footer formRecherche V1.0

--- recherche.php
+++ recherche.php
@@ -4,7 +4,7 @@
     <h1>Recherche</h1>
     <div class="row">
         <article class="panel large-12 medium-12 small-12 columns" >
-            <form action="#" method="post">
+            <form action="php/formRecherche.php" method="post">
                 <div class="row">
                     <div class="large-4 medium-4 small-4 columns"><input type="text" placeholder="Nom auteur" id="nomAuteur" name="nomAuteur"></div>
                     <div class="large-4 medium-4 small-4 columns"><input type="text" placeholder="Titre du livre" id="titreOeuvre" name="titreOeuvre"></div>
