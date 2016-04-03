4ad97021869137e79e8829be575ffb6b938340c1
cladlink <michael.boutboul87@gmail.com>
2016-04-03 14:54:00 +0200

    Requetes ok table ok foundation css + index  + header + footer formRecherche V1.0

--- Header.php
+++ Header.php
@@ -13,33 +13,39 @@
 		<script src="js/vendor/modernizr.js"></script>
 	</head>
 	<body>
-			<div class="fixed" >
-				<nav class="top-bar" data-topbar role="navigation" data-options="is_hover: true">
-					<ul class="title-area">
-						<li class="name">
-							<h1><a href="#">My Site</a></h1>
-						</li>
-						<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-						<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+		<div class="fixed" >
+			<nav class="top-bar menu " data-topbar role="navigation">
+				<ul class="title-area">
+					<li class="name">
+						<h1><a href="index.php">Bibliothèque</a></h1>
+					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+				</ul>
+                <section class="top-bar-section">
+                    <ul class="center">
+                        <li class="active"><a href="recherche.php">Recherche</a></li>
+                        <li class="active">Gestion livre</a>
+                            <ul>
+                                <li class="dropdown"><a href="ajouterLivre.php">Ajouter un livre</a></li>
+                                <li class=""></li>
+                            </ul>
+                        </li>
+                        <li class="active"><a href="emprunt.php">Gestion emprunt</a></li>
+                        <li class="active"><a href="adherent.php">Gestion adhérent</a></li>
+                    </ul>
+                </section>
+				<div class="top-bar-section">
+					<ul class="center">
+						<li class="active"><a href="recherche.php">Recherche</a></li>
+						<li class="active">Gestion livre</a>
+                            <ul>
+                                <li class="dropdown"><a href="ajouterLivre.php">Ajouter un livre</a></li>
+                                <li class=""></li>
+                            </ul>
+                        </li>
+						<li class="active"><a href="emprunt.php">Gestion emprunt</a></li>
+						<li class="active"><a href="adherent.php">Gestion adhérent</a></li>
 					</ul>
-
-					<section class="top-bar-section">
-						<!-- Right Nav Section -->
-						<ul class="right">
-							<li class="active"><a href="#">Right Button Active</a></li>
-							<li class="has-dropdown">
-								<a href="#">Right Button Dropdown</a>
-								<ul class="dropdown">
-									<li><a href="#">First link in dropdown</a></li>
-									<li class="active"><a href="#">Active link in dropdown</a></li>
-								</ul>
-							</li>
-						</ul>
-
-						<!-- Left Nav Section -->
-						<ul class="left">
-							<li><a href="#">Left Nav Button</a></li>
-						</ul>
-					</section>
-				</nav>
-			</div>
+				</div>
+			</nav>
+		</div>
